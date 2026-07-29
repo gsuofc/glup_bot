f7ca18cca750d86e27f4f637329780fc7efd2fed
mbp <113817803+gsuofc@users.noreply.github.com>
2026-07-28 23:42:31 -0600

    Nerf boost

--- main.py
+++ main.py
@@ -362,7 +362,7 @@ async def fish(interaction: discord.Interaction):
     # We have a fish, now we roll for rarity
     boost = 1
     if whenitis906():
-        boost = 4
+        boost = 3
 
     rarity_roll = fishing.roll_rarity(boost)
 
