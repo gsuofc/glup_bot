90817bf73a728d8bb760e69fa6f2cd018ba26efc
gsuofc <113817803+gsuofc@users.noreply.github.com>
2026-07-29 11:03:15 -0600

    Add 9:06 notice

--- main.py
+++ main.py
@@ -385,6 +385,9 @@ async def fish(interaction: discord.Interaction):
         last_biggest_size = fishing.convert_roll_to_weight(last_biggest_weight,fish_rolled["ave_size"])
         biggest_size_message = f"Biggest Catch: {last_biggest_size:.3f} ({last_biggest_rank})"
 
+    if whenitis906():
+        biggest_size_message+="\nWhen it is 9:06: :POG:"
+
     new_quantity = last_quantity + 1
     new_biggest_weight = last_biggest_weight
     if rarity_roll > last_biggest_weight:
